a007c16eebc4875843eb1e2276e432f9fe43e6b8
Arjun Mann <arjun7ate9@gmail.com>
2025-03-16 22:35:55 +0000

    removed pesky newline

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -89,7 +89,7 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
     if(ladder.empty()) { //No words
-        cout << "No word ladder found." << endl;
+        cout << "No word ladder found.";
     }
     for(string word : ladder) {
         cout << word + " ";
